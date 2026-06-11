963f774bfaecbf66ce6b9c6ed4fec62c4cf543d6
Василюк Матвей 350501 <vasilukmat23@gmail.com>
2026-06-11 13:03:20 +0300

    init

---
+++ src/main/java/com/matvey/innowiseorderservice/InnowiseOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.matvey.innowiseorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InnowiseOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InnowiseOrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matvey/innowiseorderservice/InnowiseOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matvey.innowiseorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnowiseOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
